ccdaea8fa0cf46c3cb1af2bb2666a8afde75f4bd
Kyungmee Kim <kkyungmee0209@gmail.com>
2022-10-27 13:14:51 +0900

    first commit

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	transpileDependencies: ['vuetify'],
+	chainWebpack: config => {
+		config.plugin('html').tap(args => {
+			args[0].title = 'Board'
+			return args
+		})
+	},
+	devServer: {
+		overlay: false,
+		// public: '0.0.0.0:8080',
+		proxy: 'http://10.1.2.6:8090'
+	},
+
+}
